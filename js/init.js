700316c5282d6f36b78f09e11b0ccbbce27cfc46
pdwp801 <nombreDelEmail>
2021-11-07 19:27:13 -0600

    Cambio

--- js/init.js
+++ js/init.js
@@ -7,6 +7,6 @@ firebase.initializeApp({
   projectId: "rfbdwp801",
   storageBucket: "rfbdwp801.appspot.com",
   messagingSenderId: "335302255418",
-  appId: "1:335302255418:web:ad30622f70830a1826a603",
-  measurementId: "G-7YJBQN5PZQ"
+  appId: "1:335302255418:web:15427d0d702517c426a603",
+  measurementId: "G-13RB54RPGY"
     });
