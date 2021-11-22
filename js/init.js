8c255632076290037edf7a69905a207f181ac7da
rdwp801 <rdwp801@gmail.com>
2021-11-22 08:35:33 -0600

    Cambio del init y subida de archivos de la aplicación completa de web

---
+++ js/init.js
@@ -0,0 +1,18 @@
+/*  Inicializa Firebase con la
+ * configuración del proyecto.
+ * Revisa la configuración en tu
+ * servidor de Firebase y cópiala
+ * aquí sustituyendo los
+ * asteriscos. Los campos deben
+ * quedar igual que en tu
+ * servidor. */
+// @ts-ignore
+firebase.initializeApp({
+  apiKey: "AIzaSyBYsPHpdYXy6jvmrRt-vNh5COOePT4GmGE",
+  authDomain: "rfbdwp801.firebaseapp.com",
+  projectId: "rfbdwp801",
+  storageBucket: "rfbdwp801.appspot.com",
+  messagingSenderId: "335302255418",
+  appId: "1:335302255418:web:15427d0d702517c426a603",
+  measurementId: "G-13RB54RPGY"
+});
